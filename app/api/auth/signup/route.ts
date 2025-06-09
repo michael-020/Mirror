a4d253acd1d016ff7c93eb12254dff3441c3f133
Michael Hosamani <michaelhosamani26@gmail.com>
2025-06-10 00:25:18 +0530

    added zod schema in signup route

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -1,10 +1,32 @@
 import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcrypt"
+import z from "zod"
+
+const signUpSchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    username: z.string(),
+    password: z.string()
+        .min(8, "Password should be at least 8 characters")
+        .max(100, "Password should not exceed 100 characters")
+        .regex(/[a-z]/, "Password must contain at least 1 lowercase letter")
+        .regex(/[A-Z]/, "Password must contain at least 1 uppercase letter")
+        .regex(/[0-9]/, "Password must contain at least 1 number")
+        .regex(/[^A-Za-z0-9]/, "Password must contain at least 1 special character")
+})
 
 export async function POST(req: NextRequest){
     try {
-        const { name, username, email, password } = await req.json()
+        const body = await req.json();
+        const checkBody = await signUpSchema.safeParse(body)
+        if(checkBody.error){
+            return NextResponse.json(
+                { msg: "Invalid credentials" },
+                { status: 400 }
+            )
+        }
+        const { name, username, email, password } = checkBody.data
         // signup
         const existingUser = await prisma.user.findUnique({
             where: {
@@ -46,7 +68,7 @@ export async function POST(req: NextRequest){
             { status: 200}
         )
     } catch (error) {
-        console.error("Error while signing in/signing up: ", error)
+        console.error("Error while signing up: ", error)
         return NextResponse.json(
             { error: "Internal server error" },
             { status: 500 }
